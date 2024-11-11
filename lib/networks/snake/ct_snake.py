c75d9b8c03ccb98ad57da66e33d22dda94a7a5e8
riverj <1321303339@qq.com>
2024-11-12 00:24:15 +0800

    remove

--- lib/networks/snake/ct_snake.py
+++ lib/networks/snake/ct_snake.py
@@ -14,12 +14,6 @@ class Network(nn.Module):
                           final_kernel=1, 
                           last_level=5,
                           head_conv=head_conv)
-        self.dla2 = DLASeg('dla{}'.format(num_layers), heads,
-                          pretrained=True,
-                          down_ratio=down_ratio/2,
-                          final_kernel=1, 
-                          last_level=5,
-                          head_conv=head_conv)
 
         self.raft = RAFT() ##Recurrent all-pairs Field transforms 也就是论文中进行迭代回归的模块
 
